bc7aaa1181e750f9d49bbeeb62ae41d10b462444
HRodriguezJimenez <harol469@gmail.com>
2023-10-19 19:15:58 -0500

    ok

--- ProyectoRickandMorty/rick-and-morty/Client-Front/src/App.js
+++ ProyectoRickandMorty/rick-and-morty/Client-Front/src/App.js
@@ -12,87 +12,87 @@ import './App.css';
 
 function App() {
 
-   const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([])
 
-   const onClose = (id) => {
-      const filtro = characters.filter((char) => char.id !== id)
-      setCharacters(filtro)
-   } 
+  const onClose = (id) => {
+    const filtro = characters.filter((char) => char.id !== id)
+    setCharacters(filtro)
+  } 
 
-   const navigate = useNavigate();
-   const [access, setAccess] = useState(false);
-   const EMAIL = "";//haroldrod1@hotmail.com
-   const PASSWORD = "";//123456
+  const navigate = useNavigate();
+  const [access, setAccess] = useState(false);
 
+  function login(userData) {
+    const { email, password } = userData;
+    const URL = 'http://localhost:3001/rickandmorty/login/';
+    axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
+        const { access } = data;
+        setAccess(data);
+        access && navigate('/home');
+    });
+  }    
+  
 
-   const login = ({ email, password }) => {
-      if (email === EMAIL && password === PASSWORD) {
-        setAccess(true);
-        navigate('/home');
-      }
-   }
-    
+  useEffect(() => {
+    !access && navigate('/');   
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [access]);
 
-   useEffect(() => {
-      !access && navigate('/');   
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [access]);
-
-   function onSearch(id) {
-      axios
-        .get(`http://localhost:3001/rickandmorty/character/${id}`)
-        .then(({ data }) => {
-          if (data.name) {
-            // Verificar si el personaje ya está en la lista
-            const isCharacterInList = characters.some((char) => char.id === data.id);
-            if (!isCharacterInList) {
-              setCharacters((oldChars) => [...oldChars, data]);
-            } else {
-              window.alert('¡Este personaje ya está en la lista!');
-            }
+  function onSearch(id) {
+    axios
+      .get(`http://localhost:3001/rickandmorty/character/${id}`)
+      .then(({ data }) => {
+        if (data.name) {
+          // Verificar si el personaje ya está en la lista
+          const isCharacterInList = characters.some((char) => char.id === data.id);
+          if (!isCharacterInList) {
+            setCharacters((oldChars) => [...oldChars, data]);
           } else {
-            window.alert('¡No hay personajes con este ID!');
+            window.alert('¡Este personaje ya está en la lista!');
           }
-        })
-        .catch((error) => {
-          if (error.response && error.response.status === 404) {
-            window.alert('¡No se encontró el personaje con este ID!');
-          } else {
-            window.alert('Ocurrió un error al buscar el personaje.');
-          }
-        });
-    }    
+        } else {
+          window.alert('¡No hay personajes con este ID!');
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          window.alert('¡No se encontró el personaje con este ID!');
+        } else {
+          window.alert('Ocurrió un error al buscar el personaje.');
+        }
+      });
+  }    
 
-   const location = useLocation();
+  const location = useLocation();
 
-   return (
-      <div className='App'>
-         
-         {
-            location.pathname.startsWith('/home') || 
-            location.pathname.startsWith('/about') || 
-            location.pathname.startsWith('/detail') ||
-            location.pathname.startsWith('/favorites') ? (
-               <Nav onSearch={onSearch} />
-            ) : null
-         }
+  return (
+    <div className='App'>
+        
+        {
+          location.pathname.startsWith('/home') || 
+          location.pathname.startsWith('/about') || 
+          location.pathname.startsWith('/detail') ||
+          location.pathname.startsWith('/favorites') ? (
+              <Nav onSearch={onSearch} />
+          ) : null
+        }
 
-         <Routes>
-            
-            <Route path='/' element={<Form login={login} />} />
+        <Routes>
+          
+          <Route path='/' element={<Form login={login} />} />
 
-            <Route path="/home" element={<Home characters={characters} onClose={onClose} />} />
+          <Route path="/home" element={<Home characters={characters} onClose={onClose} />} />
 
-            <Route path="/about" element={<About />} />  
+          <Route path="/about" element={<About />} />  
 
-            <Route path="/favorites" element={<Favorites onClose={onClose} />} />          
+          <Route path="/favorites" element={<Favorites onClose={onClose} />} />          
 
-            <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/detail/:id" element={<Detail />} />
 
-         </Routes >
-         
-      </div>
-   );
+        </Routes >
+        
+    </div>
+  );
 }
 
 
--- ProyectoRickandMorty/rick-and-morty/Client-Front/src/Redux/reducer.js
+++ ProyectoRickandMorty/rick-and-morty/Client-Front/src/Redux/reducer.js
@@ -9,35 +9,20 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {    
     switch (action.type) {
         case ADD_FAV:
-            return { ...state, 
-                myFavorites: [ ...state.allCharacters, action.payload],
-                allCharacters: [ ...state.allCharacters, action.payload],            
-            };
+            return { ...state, myFavorites: action.payload, allCharacters: action.payload };
             
         case REMOVE_FAV:
-            const isNumber = parseInt(action.payload, 10);
-            return { ...state,
-                myFavorites: state.myFavorites.filter((char) => 
-                char.id !== isNumber
-            )};
+            return { ...state, myFavorites: action.payload };
 
-            case FILTER:
-                const filteredCharacters = state.allCharacters.filter((char) => 
-                    char.gender === action.payload
-                );
-
-        //*Tambien podemos realizar el filtrado directamente en la propiedad.
-          /*case FILTER:
-                return {
-                    ...state,
-                    myFavorites: state.allCharacters.filter((char) => 
-                    char.gender === action.payload
-                }*/
-            
-                return {
-                    ...state,
-                    myFavorites: filteredCharacters, // Actualiza myFavorites con los personajes filtrados
-                };
+        case FILTER:
+            const filteredCharacters = state.allCharacters.filter((char) => 
+                char.gender === action.payload
+            );        
+        
+            return {
+                ...state,
+                myFavorites: filteredCharacters, // Actualiza myFavorites con los personajes filtrados
+            };
 
         case ORDER:
             const copyState3 = [ ...state.allCharacters];            
